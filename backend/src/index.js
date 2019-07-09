deedf8773918c3cad79348e5d8bda34cae6162cc
Yves Vinckier <yves.vinckier@live.fr>
2019-07-09 23:50:07 +0200

    User Signup in React

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
+const cookieParser = require('cookie-parser');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO Use express middlware to handle cookies (JWT)
+server.express.use(cookieParser());
 // TODO Use express middlware to populate current user
 
 server.start(
@@ -15,6 +17,6 @@ server.start(
         },
     },
     deets => {
-        console.log(`Server is now running on port http:/localhost:${deets.port}`);
+        console.log(`Server is now running on port http://localhost:${deets.port}`);
     }
 );
