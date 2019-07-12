c41c0decfa4edf609190b4b465f94ff832f7ccde
Yves Vinckier <yves.vinckier@live.fr>
2019-07-13 00:08:47 +0200

    Gated Sign In component

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -2,14 +2,23 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
+const { transport, makeANiceEmail } = require('../mail');
 
 const Mutations = {
     async createItem(parent, args, ctx, info) {
-        // TODO: Check if they are logged in
+        if (!ctx.request.userId) {
+            throw new Error('You must be logged in to do that!');
+        }
 
         const item = await ctx.db.mutation.createItem(
             {
                 data: {
+                    // This is how to create a relationship between the Item and the User
+                    user: {
+                        connect: {
+                            id: ctx.request.userId,
+                        },
+                    },
                     ...args,
                 },
             },
@@ -110,9 +119,19 @@ const Mutations = {
             where: { email: args.email },
             data: { resetToken, resetTokenExpiry },
         });
-        console.log(res);
-        return { message: 'Thanks!' };
         // 3. Email them that reset token
+        const mailRes = await transport.sendMail({
+            from: 'yves.vinckier@live.fr',
+            to: user.email,
+            subject: 'Your Password Reset Token',
+            html: makeANiceEmail(`Your Password Reset Token is here!
+      \n\n
+      <a href="${process.env
+                    .FRONTEND_URL}/reset?resetToken=${resetToken}">Click Here to Reset</a>`),
+        });
+
+        // 4. Return the message
+        return { message: 'Thanks!' };
     },
     async resetPassword(parent, args, ctx, info) {
         // 1. check if the passwords match
