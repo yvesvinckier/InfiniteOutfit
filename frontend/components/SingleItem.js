1510db2f10b10dc06ac458269f37cb5663d697cf
Yves Vinckier <yves.vinckier@live.fr>
2019-07-21 22:30:53 +0200

    Testing and Mocking Appolo Queries

--- frontend/components/SingleItem.js
+++ frontend/components/SingleItem.js
@@ -35,35 +35,36 @@ const SINGLE_ITEM_QUERY = gql`
   }
 `;
 class SingleItem extends Component {
-    render() {
-        return (
-            <Query
-                query={SINGLE_ITEM_QUERY}
-                variables={{
-                    id: this.props.id,
-                }}
-            >
-                {({ error, loading, data }) => {
-                    if (error) return <Error error={error} />;
-                    if (loading) return <p>Loading...</p>;
-                    if (!data.item) return <p>No Item Found for {this.props.id}</p>;
-                    const item = data.item;
-                    return (
-                        <SingleItemStyles>
-                            <Head>
-                                <title>Sick Fits | {item.title}</title>
-                            </Head>
-                            <img src={item.largeImage} alt={item.title} />
-                            <div className="details">
-                                <h2>Viewing {item.title}</h2>
-                                <p>{item.description}</p>
-                            </div>
-                        </SingleItemStyles>
-                    );
-                }}
-            </Query>
-        );
-    }
+  render() {
+    return (
+      <Query
+        query={SINGLE_ITEM_QUERY}
+        variables={{
+          id: this.props.id,
+        }}
+      >
+        {({ error, loading, data }) => {
+          if (error) return <Error error={error} />;
+          if (loading) return <p>Loading...</p>;
+          if (!data.item) return <p>No Item Found for {this.props.id}</p>;
+          const item = data.item;
+          return (
+            <SingleItemStyles>
+              <Head>
+                <title>Sick Fits | {item.title}</title>
+              </Head>
+              <img src={item.largeImage} alt={item.title} />
+              <div className="details">
+                <h2>Viewing {item.title}</h2>
+                <p>{item.description}</p>
+              </div>
+            </SingleItemStyles>
+          );
+        }}
+      </Query>
+    );
+  }
 }
 
 export default SingleItem;
+export { SINGLE_ITEM_QUERY };
