d26ee41f3a49ba7b9667e5f34158fc36feb65395
yves <yves.vinckier@live.fr>
2019-08-30 16:53:35 +0200

    header

--- frontend/components/styles/NavStyles.js
+++ frontend/components/styles/NavStyles.js
@@ -1,11 +1,11 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const NavStyles = styled.ul`
   margin: 0;
   padding: 0;
   display: flex;
   justify-self: end;
-  font-size: 2rem;
+  font-size: 1.2rem;
   a,
   button {
     padding: 1rem 3rem;
@@ -13,39 +13,26 @@ const NavStyles = styled.ul`
     align-items: center;
     position: relative;
     text-transform: uppercase;
-    font-weight: 900;
+    font-weight: 400;
     font-size: 1em;
     background: none;
     border: 0;
     cursor: pointer;
     color: ${props => props.theme.black};
-    font-weight: 800;
     @media (max-width: 700px) {
       font-size: 10px;
       padding: 0 10px;
     }
-    &:before {
-      content: '';
-      width: 2px;
-      background: ${props => props.theme.lightgrey};
-      height: 100%;
-      left: 0;
-      position: absolute;
-      transform: skew(-20deg);
-      top: 0;
-      bottom: 0;
-    }
     &:after {
-      height: 2px;
-      background: red;
-      content: '';
+      height: 1px;
+      background: ${props => props.theme.black};
+      content: "";
       width: 0;
       position: absolute;
       transform: translateX(-50%);
-      transition: width 0.4s;
-      transition-timing-function: cubic-bezier(1, -0.65, 0, 2.31);
+      transition: width 0.4s ease-out;
       left: 50%;
-      margin-top: 2rem;
+      margin-top: 1.2rem;
     }
     &:hover,
     &:focus {
@@ -59,7 +46,7 @@ const NavStyles = styled.ul`
     border-top: 1px solid ${props => props.theme.lightgrey};
     width: 100%;
     justify-content: center;
-    font-size: 1.5rem;
+    font-size: 1rem;
   }
 `;
 
